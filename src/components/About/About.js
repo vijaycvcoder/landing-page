81a76ccba355e98cac41a2e78e9859e5441129c1
vijaycvcoder <vijayasundaram96bosch@gmail.com>
2025-04-09 08:40:33 +0530

    update the about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,21 +22,24 @@ const About = ({ onSkillSelect }) => {
             <img src={require('./icons/data-analyst.png')} alt="Data analyst" />
             <div className={styles.aboutItemText}>
               <h3>Data Analyst</h3>
-              <p>I have an experience in cleaning and formatting the data using POWER BI</p>
+              <p>I specialize in transforming complex datasets into clear,
+                actionable insights that empower to make informed decisions</p>
             </div>
           </li>
           <li className={styles.aboutItem} onClick={() => handleSkillClick('ai')}>
             <img src={require('./icons/machine-learning.png')} alt="Machine Learning" />
             <div className={styles.aboutItemText}>
               <h3>AI/ML Developer</h3>
-              <p>I have experience developing ML Apps,Deep Learning Apps using Natural Language Processing.</p>
+              <p>I design and deploy innovative machine learning models that transform
+              raw data into intelligent, real-world applications.</p>
             </div>
           </li>
           <li className={styles.aboutItem} onClick={() => handleSkillClick('ui')}>
             <img src={require('./icons/designer.png')} alt="Designer" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>I have designed multiple Posters using canvas.</p>
+              <h3>Graphic Designer</h3>
+              <p>I create powerful visual experiences that communicate ideas,
+                evoke emotions, and leave a lasting impact.</p>
             </div>
           </li>
         </ul>
